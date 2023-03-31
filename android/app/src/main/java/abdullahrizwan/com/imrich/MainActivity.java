dde70b95c8caa6177c6a31c1d1206f55d3046fd8
Abdullah Rizwan <100567651+BOYYY743@users.noreply.github.com>
2023-03-31 12:57:06 +0500

    Initial commit

---
+++ android/app/src/main/java/abdullahrizwan/com/imrich/MainActivity.java
@@ -0,0 +1,6 @@
+package abdullahrizwan.com.imrich;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/practice.dart
@@ -0,0 +1,13 @@
+
+void main()
+{
+  print('objext');
+  int x = addSum(1,2);
+  print(x);
+}
+
+int addSum(int a,int b)
+{
+  return a+b;
+}
+
