9a855340daca91e7474f9139c3ce5acbd75b8798
Abdullah Rizwan <100567651+BOYYY743@users.noreply.github.com>
2023-03-31 13:04:28 +0500

    basic dart programs

--- lib/practice.dart
+++ lib/practice.dart
@@ -1,9 +1,23 @@
 
+import 'dart:io';
+
 void main()
 {
-  print('objext');
   int x = addSum(1,2);
   print(x);
+
+  for(int i = 0;i<5;i++)
+    {
+      print('hello ${i + 1}');
+    }
+
+  String? y;
+  stdout.write("Enter name: ");
+  y = stdin.readLineSync();
+
+  print("Hey,$y");
+
+
 }
 
 int addSum(int a,int b)
